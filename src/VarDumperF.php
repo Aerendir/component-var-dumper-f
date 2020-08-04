ae4c643126d0a51fcabbcc039f782345d12894cb
Aerendir <hello@aerendir.me>
2020-08-04 13:14:48 +0200

    Apply style fixes.

--- src/VarDumperF.php
+++ src/VarDumperF.php
@@ -3,13 +3,12 @@
 declare(strict_types=1);
 
 /*
- * This file is part of VarDumper CLI to HTML.
+ * This file is part of the Serendipity HQ VarDumper F Component.
  *
- * Copyright Adamo Aerendir Crespi 2020.
+ * Copyright (c) Adamo Aerendir Crespi <aerendir@serendipityhq.com>.
  *
- * @author    Adamo Aerendir Crespi <hello@aerendir.me>
- * @copyright Copyright (C) 2020 Aerendir. All rights reserved.
- * @license   MIT
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace SerendipityHQ\Component\VarDumperCliToHtml;
